9aaf6dc0e23f32ada8f6a703f044b5e8bd8233c9
Antoine MASSY <anmassy@made-f0Cr9s5.clusters.42paris.fr>
2023-12-16 12:24:35 +0100

    first push

--- Cpp_Module01/ex06/harl.cpp
+++ Cpp_Module01/ex06/harl.cpp
@@ -6,7 +6,7 @@
 /*   By: anmassy <anmassy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 12:49:37 by anmassy           #+#    #+#             */
-/*   Updated: 2023/12/15 17:32:34 by anmassy          ###   ########.fr       */
+/*   Updated: 2023/12/16 12:23:43 by anmassy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,41 +22,45 @@ harl::~harl(void) {
 
 void	harl::debug(void) {
 	
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
 }
 
 void	harl::info(void) {
-
+	
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
 }
 
 void	harl::warning(void) {
 
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free.I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	harl::error(void) {
 	
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
 
 void	harl::complain(std::string level) {
-	// t_fonc fonc[] = {&harl::debug, &harl::info, &harl::warning, &harl::error};
+	t_fonc fonc[4] = {&harl::debug, &harl::info, &harl::warning, &harl::error};
 	std::string word[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	int i = 0;
-	while (i < 4 && !word[i].compare(level))
+	while (i < 4 && word[i].compare(level))
 		i++;
 	switch (i) {
-	case 0:
-		this->debug();
-	case 1:
-		this->info();
-	case 2:
-		this->warning();
-	case 3:
-		this->error();
-		break ;
-	default:
+		case 0:
+			(this->*fonc[1])();
+		case 1:
+			(this->*fonc[1])();
+		case 2:
+			(this->*fonc[2])();
+		case 3:
+			(this->*fonc[3])();
+			break ;
+		default:
 			std::cout << "incorrect argument" << std::endl;
 	}
 }
